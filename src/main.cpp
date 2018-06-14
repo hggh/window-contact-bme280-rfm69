605550eb958d8ab6f685959ac371884f161f24a7
Jonas Genannt <jonas@brachium-system.net>
2018-06-15 00:33:41 +0200

    update code

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@
 #include <JeeLib.h>
 
 
+// reed contact 1 = RE1/P3 = window fully open
+#define REED_CONTACT_1 A1
+
+// reed contact 2 = RE2/P5 = window half open
+#define REED_CONTACT_2 A0
+
 #define NODEID        101
 #define NETWORKID     100
 #define GATEWAYID     1
@@ -12,12 +18,45 @@
 #define ACK_TIME      15
 #define DEBUG 1
 
+#define WINDOW_STATUS_UNDEFINED 0
+#define WINDOW_STATUS_CLOSED    1
+#define WINDOW_STATUS_HALF_OPEN 2
+#define WINDOW_STATUS_OPENED    3
+
+#define ACTION_STATUS_SLEEP 0
+#define ACTION_STATUS_READ 1
+
 RFM69 radio;
 byte ADCSRA_status;
 const float InternalReferenceVoltage = 1.1;
+volatile short window_status = WINDOW_STATUS_UNDEFINED;
+volatile short action_status = ACTION_STATUS_SLEEP;
 
 ISR(WDT_vect) {
 	Sleepy::watchdogEvent();
+}
+
+ISR(PCINT1_vect) {
+	action_status = ACTION_STATUS_READ;
+}
+
+short get_window_status() {
+	bool reed_status_1 = digitalRead(REED_CONTACT_1);
+	bool reed_status_2 = digitalRead(REED_CONTACT_2);
+
+	if (reed_status_1 == false and reed_status_2 == false) {
+		return WINDOW_STATUS_CLOSED;
+	}
+
+	if (reed_status_1 == true and reed_status_2 == false) {
+		return WINDOW_STATUS_OPENED;
+	}
+
+	if (reed_status_1 == false and reed_status_2 == true) {
+		return WINDOW_STATUS_HALF_OPEN;
+	}
+
+	return WINDOW_STATUS_UNDEFINED;
 }
 
 float read_battery_volatage() {
@@ -42,8 +81,44 @@ float read_battery_volatage() {
 	return battery_voltage;
 }
 
-void setup()
-{
+void setup_pin_change_interrupt() {
+	noInterrupts();
+	PCICR |= (1 << PCIE1);
+
+	PCMSK1 |= (1<<PCINT8);
+	PCMSK1 |= (1<<PCINT9);
+
+	interrupts();
+}
+
+void disable_pin_change_interrupt() {
+	noInterrupts();
+
+	PCICR |= ~(1 << PCIE1);
+	PCMSK1 |= ~(1<<PCINT8);
+	PCMSK1 |= ~(1<<PCINT9);
+
+	interrupts();
+}
+
+void send_status(short status) {
+	char buffer[19];
+	float volt = (double)read_battery_volatage();
+
+	char volt_str[10];
+	dtostrf(volt, 3,2, volt_str);
+
+	sprintf(buffer, "WindowStatus:%d:%s", status, volt_str);
+	radio.sendWithRetry(GATEWAYID, buffer, strlen(buffer), 6);
+	radio.sleep();
+
+	if (DEBUG == 1) {
+		Serial.println(buffer);
+		Serial.flush();
+	}
+}
+
+void setup() {
 	if (DEBUG == 1) {
 		Serial.begin(9600);
 		Serial.println("Starting window-contact-rfm69");
@@ -52,26 +127,50 @@ void setup()
 	else {
 		power_usart0_disable();
 	}
-	// disable ADC
 	ADCSRA_status = ADCSRA;
 	ADCSRA &= ~(1 << 7);
 	power_adc_disable();
+	power_twi_disable();
+
+	pinMode(REED_CONTACT_1, INPUT);
+	pinMode(REED_CONTACT_2, INPUT);
 
 	radio.initialize(FREQUENCY, NODEID, NETWORKID);
 	radio.encrypt(ENCRYPTKEY);
 	radio.sleep();
+
+	window_status = get_window_status();
+	send_status(window_status);
+
+	setup_pin_change_interrupt();
 }
 
 void loop() {
-	float volt = (double)read_battery_volatage();
+	if (action_status == ACTION_STATUS_READ) {
+		disable_pin_change_interrupt();
 
-	char buffer[32];
-	char volt_str[10];
-	dtostrf(volt, 3,2, volt_str);
+		if (DEBUG == 1) {
+			Serial.println("at action_status == ACTION_STATUS_READ");
+			Serial.flush();
+		}
 
-	sprintf(buffer, "Test:%s", volt_str);
-	radio.sendWithRetry(GATEWAYID, buffer, strlen(buffer), 2);
-	radio.sleep();
+		Sleepy::loseSomeTime(3000);
+		if (DEBUG == 1) {
+			Serial.println("at action_status == ACTION_STATUS_READ - after sleep");
+			Serial.flush();
+		}
 
-	Sleepy::loseSomeTime(5000);
+		window_status = get_window_status();
+		send_status(window_status);
+
+		action_status = ACTION_STATUS_SLEEP;
+	}
+	if (action_status == ACTION_STATUS_SLEEP) {
+		if (DEBUG == 1) {
+			Serial.println("at action_status == ACTION_STATUS_SLEEP");
+			Serial.flush();
+		}
+		setup_pin_change_interrupt();
+		Sleepy::powerDown();
+	}
 }
